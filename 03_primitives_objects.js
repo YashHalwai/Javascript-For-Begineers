65b5764f39bf42983e21e22f71a6cd4f0fb446d0
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-08-14 15:33:58 +0530

    primitives object

---
+++ 03_primitives_objects.js
@@ -0,0 +1,24 @@
+// NN BB SS U
+
+let a = null
+let b = 75;
+let c = true;
+let d = BigInt("567") + BigInt(3)
+let e = "Harry"
+let f = Symbol("I am a nice symbol")
+let g 
+console.log(a,b,c,d,e,f,g)
+console.log(typeof d)
+
+// Non Primitives Datatypes - - > Objects in JS
+
+const item = 
+  {
+    "Harry" : true,
+    "Shubh" : false,
+    "Lovish" : 67,
+     "Rohan" : undefined
+  }
+console.log(item["Harry"])
+console.log(item["harry"])
+console.log(item["Shubh"])
