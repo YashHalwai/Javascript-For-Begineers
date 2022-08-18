1a02e7438b302b9353064523e1ee2e3c6378ff05
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-08-18 22:36:10 +0530

    operators

---
+++ 05_Operators.js
@@ -0,0 +1,80 @@
+console.log("Operators in JS")
+
+// Arithmetic Operators
+
+// let a = 45;
+// let b = 4;
+
+// console.log("a + b = ", a+b);
+// console.log("a - b = ", a-b);
+// console.log("a / b = ", a/b);
+// console.log("a * b = ", a*b);
+// console.log("a ** b = ", a**b);
+// console.log("a % b = ", a%b);
+
+// console.log("++a = ", ++a);
+// console.log("a = ", a);
+
+// console.log("a++ = ", a++);
+// console.log("a = ", a);
+
+// console.log("--a = ", --a);
+// console.log("a = ", a);
+
+// console.log("a-- = ", a--);
+// console.log("a = ", a);
+
+
+// Assignment Operators
+
+
+// let assignment = 1;
+// assignment += 5 // same as assignment = assignment + 5
+// console.log("a is now = ", assignment)
+
+// assignment -= 5 // same as assignment = assignment - 5
+// console.log("a is now = ", assignment)
+
+// assignment *= 5 // same as assignment = assignment * 5
+// console.log("a is now = ", assignment)
+
+// assignment /= 5 // same as assignment = assignment / 5
+// console.log("a is now = ", assignment)
+
+// assignment %= 5 // same as assignment = assignment / 5
+// console.log("a is now = ", assignment)
+
+// assignment **= 5 // same as assignment = assignment / 5
+// console.log("a is now = ", assignment)
+
+
+// Comaparison Operators
+
+// let comp1 = 6;
+// let comp2 = 8;
+
+// console.log("comp1 == comp2 = ", comp1 == comp2)
+// console.log("comp1 != comp2 = ", comp1 != comp2)
+// console.log("comp1 === comp2 = ", comp1 === comp2) // type
+// console.log("comp1 !== comp2 = ", comp1 !== comp2)
+// console.log("comp1 !== comp2 = ", comp1 !== comp2)
+// console.log("comp1 < comp2 = ", comp1 < comp2)
+
+
+// Logical Opeators
+
+// let x  = 5;
+// let y = 6;
+
+// console.log(x < y && x <= 5) // && (true && true = true) otherwise false
+
+// console.log(x > y || x <= 5) // || (true || false = true , false || true = true, true || true = true) 0 || 0 = 0
+
+// Single Line Comment
+
+/*
+Multiple
+Line 
+Comment
+*/
+
